1204dd0149f9c83bc57ed0c8eb417de3f3e9fd60
ysanematsu <sanematsuyasuo@gmail.com>
2020-08-01 16:03:25 +0900

    課題：8月1日　MessageView追加

---
+++ Instagram/MessageView.swift
@@ -0,0 +1,25 @@
+//
+//  MessageView.swift
+//  Instagram
+//
+//  Created by 實松保雄 on 2020/07/29.
+//  Copyright © 2020 ysanematsu. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import SVProgressHUD
+
+class MessageView: UIViewController {
+    
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    
+   //課題：送信ボタンを押して、コメントをFirebaseに送信
+    @IBAction func commentPost(_ sender: Any) {
+    
+        //HomeViewcontrollerで取得したidを指定してコメントを送信
+    
+    }
+}
